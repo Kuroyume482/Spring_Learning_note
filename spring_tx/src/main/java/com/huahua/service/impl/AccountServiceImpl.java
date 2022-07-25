a543f6bbf6b9b4ec4578fcc561d2099e49c18cd3
Huahua <1470281992@qq.com>
2022-07-25 12:23:01 +0800

    spring_tx 基于springAOP的事务管理，

---
+++ spring_tx/src/main/java/com/huahua/service/impl/AccountServiceImpl.java
@@ -0,0 +1,29 @@
+package com.huahua.service.impl;
+
+
+import com.huahua.dao.AccountDao;
+import com.huahua.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service("accountService")
+@Transactional(isolation = Isolation.REPEATABLE_READ)
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED)
+    public void transfer(String outMan, String inMan, double money) {
+        accountDao.out(outMan,money);
+        int i = 1/0;
+        accountDao.in(inMan,money);
+    }
+}
